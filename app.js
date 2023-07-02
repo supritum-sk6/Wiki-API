690ff93cb8de34479429e651cad849425a7aa63d
supritum-sk6 <supritum21@itk.ac.in>
2023-07-02 11:56:52 +0800

    complete RESTful API

--- app.js
+++ app.js
@@ -23,6 +23,58 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model("Article", articleSchema);
 
 
+//actual server requests
+app.route("/articles")
+.get(function(req, res){
+    Article.find().then(function(foundArticles){
+        res.send(foundArticles);
+    });
+})
+.post(function(req, res){
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save().then(function(insertedArticles){
+        res.send(insertedArticles);
+    });
+})
+.delete(function(req, res){
+    Article.deleteMany().then(function(deletedArticles){
+        res.send(deletedArticles);
+    });
+});
+
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+    Article.find({title: req.params.articleTitle}).then(function(foundArticle){
+        if(foundArticle.length!=0){
+            res.send(foundArticle);
+        }else{
+            res.send("no article with the title found");
+        }
+        
+    });
+})
+.put(function(req, res){
+    Article.replaceOne({title: req.params.articleTitle}, req.body).then(function(updatedArticle){
+        res.send(updatedArticle);
+    });
+})
+.patch(function(req, res){
+    Article.updateOne({title: req.params.articleTitle}, {$set: req.body}).then(function(updatedArticle){
+        res.send(updatedArticle);
+    });
+})
+.delete(function(req, res){
+    Article.deleteMany({title: req.params.articleTitle}).then(function(deletedArticle){
+        res.send(deletedArticle);
+    });
+});
+
+
+
+
 //server requests
 app.get("/", function(req, res){
     res.send("done");
